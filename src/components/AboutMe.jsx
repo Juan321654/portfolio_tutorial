6a3b05ada1f9579a31f3bcb24cdb21ed087af0ef
juan camacho <juanc@tiv.solutions>
2021-07-08 21:42:18 -0400

    added google font and about me header

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,6 +8,7 @@ function AboutMe() {
         <img src={person}/>
       </div>
       <div className="about__text">
+        <h1>ABOUT ME</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam facilis esse quisquam incidunt, sit temporibus eaque cum nisi dolor tempore et, accusamus aliquam eveniet nam, exercitationem libero! Atque, sint labore!,consectetur, adipisicing elit. Ipsam facilis esse quisquam incidunt, sit temporibus eaque</p>
       </div>
     </div>
