8f497a746b1a5d29b2cc8abc924204ebd1c5ec2e
juan camacho <juanc@tiv.solutions>
2021-07-17 16:27:46 -0400

    portfolio project pics working fine with media queries

--- src/screens/portfolio/Portfolio.jsx
+++ src/screens/portfolio/Portfolio.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './portfolio.css'
-import Button from 'react-bootstrap/Button'
 import PopupModal from './PopupModal'
 import portfolioData from '../../helpers/portfolioData'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Image from 'react-bootstrap/Image'
 
@@ -14,21 +12,18 @@ function Portfolio() {
 
   const mapped = portfolioData.map((e, idx, arr) => {
     return (
-      <Card style={{ width: '50%'}} fluid="sm">
-        <Image className="portfolio__image" onClick={() => setModalShow(true)} src={e.image}/>
+      <Card id="portfolio__card__container">
+        <Image className="portfolio__image" onClick={() => setModalShow(true)} src={e.image} />
+        <div className="portfolio__click__info">&#x1F50E;&#xFE0E;</div>
       </Card>
     )
   })
 
   return (
-    <Container fluid="lg" style={{padding: '2rem 0'}}>
+    <Container fluid="lg" style={{ padding: '2rem 0' }}>
       <Row>
         {mapped}
       </Row>
-      {/* <Button variant="primary" onClick={() => setModalShow(true)}>
-        Launch vertically centered modal
-      </Button> */}
-
       <PopupModal
         show={modalShow}
         onHide={() => setModalShow(false)}
